83eb02015acc72eecfc1e941dd0562b0e5b49348
Tarcísio Pereira <61057709+tarcisio-pereira@users.noreply.github.com>
2025-01-12 19:40:55 -0300

    fix: teste falhando

--- person.test.js
+++ person.test.js
@@ -1,22 +1,22 @@
 const { Person } = require("./person"); // Substitua pelo caminho correto
 
 describe("Person class", () => {
-  test("should create a person instance with the given name", () => {
+  test("1. should create a person instance with the given name", () => {
     const person = new Person("Tarcísio");
     expect(person.name).toBe("Tarcísio");
   });
 });
 
 describe("Person class", () => {
-  test("should create a person instance with the given name", () => {
+  test("2. should create a person instance with the given name", () => {
     const person = new Person("Carlos");
     expect(person.name).toBe("Carlos");
   });
 });
 
 describe("Person class", () => {
-  test("should create a person instance with the given name", () => {
+  test("3. should create a person instance with the given name", () => {
     const person = new Person("Pedro");
-    expect(person.name).toBe("Carlos");
+    expect(person.name).toBe("Pedro");
   });
 });
