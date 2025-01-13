9852ac4a3e31f13b07dea832d4fb9b07d8588537
Tarcísio Pereira <61057709+tarcisio-pereira@users.noreply.github.com>
2025-01-13 19:42:08 -0300

    config: configuracao mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const { Person } = require("./person");
+// const { Person } = require("./person");
 
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectToDatabase = require("./src/database/connect");
+
+connectToDatabase();
+
+require("./modules/express");
+// require("./modules/path");
 // require("./modules/path");
 // require("./modules/fs");
 // require("./modules/http");
-require("./modules/express");
 // const person = new Person("Tarso");
